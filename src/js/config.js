e312b4921b974066ee805bfec3e967661be007cd
SethMilchick <martiskalenda@gmail.com>
2026-06-07 18:32:18 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cd523bab-0806-4240-af46-9ea2f3490850';
+export const MODAL_CLOSE_SEC = 2.5;
